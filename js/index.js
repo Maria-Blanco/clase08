25630fc3a5e006c6d34e7ce43085ddf413d92030
Maria-Blanco <mblanco6@unica.edu.ni>
2021-05-13 19:41:57 -0600

    05

---
+++ js/index.js
@@ -0,0 +1,35 @@
+/*window.addEventListener('scroll', function(){
+    let altoPantalla = window.innerHeight;
+    let anchoPantalla = window.innerWidth;
+    console.log ('el tamano de la pantalla es:' + altoPantalla + 'x' + anchoPantalla);
+
+    let objeto = document.getElementById('atle');
+    let posicionObj = objeto.getBoundingClientRect().top;
+
+    if (posicionObj < 250) {
+                                        objeto.style.opacity = '1'
+                                    }
+                                    else{
+                                        objeto.style.opacity = '0'
+                                        }
+    
+});
+window.addEventListener('scroll',function(){
+    let altoPantalla = window.innerHeight;
+    let anchoPantalla = window.innerWidth;
+    console.log ('el tamano de la pantalla es:' + altoPantalla + 'x' + anchoPantalla);
+
+    let objeto = document.getElementById('atle');
+    let posicionObj = objeto.getBoundingClientRect().top;
+
+    let objeto = document.getElementById('cicl');
+    let posicionObj = objeto.getBoundingClientRect().top;
+                                
+    if (posicionObj < 250) {
+                                        objeto.style.opacity = '1'
+                            }
+                            else{
+                                    objeto.style.opacity = '0'
+                                }
+}
+)*/
